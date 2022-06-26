31af8e36d5f83887b81e44fdb8d230b15eea1006
Rodrigo Henrique da Silva <rodrigohs007@outlook.com>
2022-06-26 20:38:45 -0300

    Upload de arquivo em um FTP de maneria simples usando C#, podendo ser inserido em qualquer projeto feito na mesma linguagem.

---
+++ Upload_FTP/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Upload_FTP
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_caminho = new System.Windows.Forms.TextBox();
+            this.txt_login = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_senha = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.txt_arquivo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Caminho do FTP";
+            // 
+            // txt_caminho
+            // 
+            this.txt_caminho.Location = new System.Drawing.Point(16, 77);
+            this.txt_caminho.Name = "txt_caminho";
+            this.txt_caminho.Size = new System.Drawing.Size(405, 23);
+            this.txt_caminho.TabIndex = 1;
+            // 
+            // txt_login
+            // 
+            this.txt_login.Location = new System.Drawing.Point(15, 128);
+            this.txt_login.Name = "txt_login";
+            this.txt_login.Size = new System.Drawing.Size(158, 23);
+            this.txt_login.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Login";
+            // 
+            // txt_senha
+            // 
+            this.txt_senha.Location = new System.Drawing.Point(211, 128);
+            this.txt_senha.Name = "txt_senha";
+            this.txt_senha.Size = new System.Drawing.Size(210, 23);
+            this.txt_senha.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(211, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Senha";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 232);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(486, 36);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Subir o Arquivo para o FTP";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(355, 185);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Selecione o Arquivo";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // txt_arquivo
+            // 
+            this.txt_arquivo.Location = new System.Drawing.Point(11, 185);
+            this.txt_arquivo.Name = "txt_arquivo";
+            this.txt_arquivo.Size = new System.Drawing.Size(339, 23);
+            this.txt_arquivo.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Caminho do Arquivo";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(12, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(263, 30);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Upload de Arquivo Simples";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 292);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_arquivo);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_senha);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_login);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_caminho);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Upload de Arquivos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_caminho;
+        private System.Windows.Forms.TextBox txt_login;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_senha;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox txt_arquivo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ Upload_FTP/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Upload_FTP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            try
+            {
+                FileInfo ArquivoInfo = new FileInfo(txt_arquivo.Text);
+                string caminho_completo = txt_caminho.Text; ;
+                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(new Uri(caminho_completo + "/" + ArquivoInfo.Name));
+
+                request.Method = WebRequestMethods.Ftp.UploadFile;
+                request.Credentials = new NetworkCredential(txt_login.Text, txt_senha.Text);
+                request.UseBinary = true;
+                request.ContentLength = ArquivoInfo.Length;
+
+                using (FileStream fs = ArquivoInfo.OpenRead())
+                {
+                    byte[] buffer = new byte[2040];
+                    int bytesSent = 0;
+                    int bytes = 0;
+
+                    using (Stream stream = request.GetRequestStream())
+                    {
+                        while (bytesSent < ArquivoInfo.Length)
+                        {
+                            bytes = fs.Read(buffer, 0, buffer.Length);
+                            stream.Write(buffer, 0, bytes);
+                            bytesSent += bytes;
+                        }
+                    }
+                }
+
+                MessageBox.Show("Arquivo, " + ArquivoInfo.Name + ", foi inserido com sucesso");
+            }
+
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Erro do tentar inserir o arquivo no FTP, segue o erro para análise:  " + ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            {
+                using (OpenFileDialog openFileDialog = new OpenFileDialog())
+                {
+
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+
+                        FileInfo fileInfo = new FileInfo(openFileDialog.FileName);
+                        //Mostra o nome do arquivo
+                        //-- fileInfo.Name;
+                        //Mostra a extensão do arquivo
+                        //--fileInfo.Extension;
+                        //Mostra o caminho completo do arquivo junto com o nome
+                        //-- fileInfo.FullName;
+
+                        txt_arquivo.Text = fileInfo.FullName;
+                    }
+                }
+            }
+        }
+    }
+}
